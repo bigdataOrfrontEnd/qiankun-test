49fa99df32074da7a59aa5bd0ec342000e86765a
17639638825 <562985990@qq.com>
2023-07-18 16:18:01 +0800

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
-
+import MachineRoom from "./component/MachineRoom";
 function App() {
+  //机房对象
+  let room: MachineRoom;
+  const chartRef: any = useRef(null); //创建一个DOM
+  chartRef.width = window.innerWidth;
+  chartRef.height = window.innerHeight;
+  room = new MachineRoom(chartRef.current);
+
   return (
     <div className="App">
-      <canvas id="canvas"></canvas>
+      111
+      <canvas ref={chartRef}></canvas>
     </div>
   );
 }
